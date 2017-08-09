fd83c799a893514c6d8012d9319c40ad0510bd56
Scriptim <Scriptim@gmx.de>
2017-08-09 15:14:00 +0200

    element name and description

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inputarea = $('#inputarea')
+const properties = $('#properties')
 
 inputarea.sortable({
   revert: true
@@ -7,10 +8,27 @@ inputarea.sortable({
 inputarea.disableSelection()
 
 $('#elements > span').click(function () {
-  inputarea.append('<span>' + $(this).html() + '<i class="fa-minus"></i></span>')
+  inputarea.append('<span data-description="' + $(this).attr('data-description') + '">' + $(this).html() + '<i class="fa-minus"></i></span>')
   var new_element = $('#inputarea > span').last()
   new_element.disableSelection()
+  new_element.click(function () {
+    $('#inputarea > span').removeAttr('data-selected')
+    new_element.attr('data-selected', 'true')
+    updateProperties(new_element)
+  })
   new_element.children('i').click(function () {
     $(this).parent().remove()
+    updateProperties()
   })
 })
+
+function updateProperties(element) {
+  properties.empty()
+
+  if (!element) {
+    return
+  }
+
+  properties.append('<h2>' + element.html().replace(/\s*<i.*<\/i>/gi, '') + '</h2 > ')
+  properties.append('<p id="description">' + element.attr('data-description') + '</p>')
+}
