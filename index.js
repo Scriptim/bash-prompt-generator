57afb4b819892adf82e151fd3eb60c872c0c2997
Scriptim <Scriptim@gmx.de>
2017-08-09 14:12:48 +0200

    remove from inputarea button

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const inputarea = $('#inputarea')
 
 $('#elements > span').click(function () {
   inputarea.append('<span>' + $(this).html() + '<i class="fa-minus"></i></span>')
+  $('#inputarea > span').last().children('i').click(function () {
+    $(this).parent().remove()
+  })
 })
