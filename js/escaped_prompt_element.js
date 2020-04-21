52982123bd1f40956485e0a88c2d3d922b78e532
Scriptim <Scriptim@gmx.de>
2020-04-21 23:19:44 +0200

    Add prompt.js and escaped_prompt_element.js

---
+++ js/escaped_prompt_element.js
@@ -0,0 +1,51 @@
+/* global Ansi */
+
+/**
+ * A single element of a prompt with colors and display attributes.
+ */
+class EscapedPromptElement {
+  /**
+   * Creates an instance of PromptElement.
+   * @param {PromptElement} content The prompt element.
+   * @param {Color} [fgColor] The foreground color.
+   * @param {Color} [bgColor] The background color.
+   * @param {Array<Ansi>} [displayAttribs] A list of SGR display attributes to apply to this
+   * element. Do not pass codes that reset attributes (such as {@link Ansi.RESET} or
+   * {@link Ansi.BOLD_DIM_OFF}).
+   * @param {string} [data] Additional data that may be required, e. g. the text of a "Text"
+   * element or the format of a "Date (formatted)" element.
+   * @memberof PromptElement
+   */
+  constructor(content, fgColor, bgColor, displayAttribs, data) {
+    this.content = content;
+    this.fgColor = fgColor;
+    this.bgColor = bgColor;
+    this.displayAttribs = displayAttribs || [];
+    if (data) {
+      this.content.replace(/~/g, data).replace(/\\/g, '\\\\');
+    }
+  }
+
+  /**
+   * Returns the string representation of this element including escape sequences
+   */
+  toString() {
+    const escapeCodes = [Ansi.RESET];
+    escapeCodes.push(...this.displayAttribs);
+    if (this.fgColor !== undefined) {
+      if (this.fgColor.id < 16) {
+        escapeCodes.push(this.fgColor.id);
+      } else {
+        escapeCodes.push([38, 5, this.fgColor.id]);
+      }
+    }
+    if (this.bgColor !== undefined) {
+      if (this.bgColor.id < 16) {
+        escapeCodes.push(this.bgColor.id);
+      } else {
+        escapeCodes.push([48, 5, this.fgColor.id]);
+      }
+    }
+    return `\\[\\e[${escapeCodes.join(';')}m\\]${this.content}`;
+  }
+}
---
+++ js/prompt.js
@@ -0,0 +1,22 @@
+/**
+ * Represents a prompt and provides methods for manipulation.
+ */
+class Prompt {
+  constructor() {
+    this.elements = [];
+  }
+
+  /**
+   * Adds an element to the end of the prompt.
+   *
+   * @param {PromptElement} element The element to be added.
+   * @memberof Prompt
+   */
+  appendElement(element) {
+    this.elements.push(element);
+  }
+
+  toString() {
+    return `PS1="${this.elements.map((el) => el.toString()).join('').replace(/"/g, '\\"')}"`;
+  }
+}
