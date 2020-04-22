652644fc9bbca8a7697a90a334b1979a8fc1b2c8
Scriptim <Scriptim@gmx.de>
2020-04-22 20:10:06 +0200

    Make added elements sortable via drag n drop

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-/* global $, PromptElement */
+/* global $, Sortable, PromptElement */
 
 const flex1 = $('#flex1');
 const flex2 = $('#flex2');
 
 function addElementInputs() {
+  // eslint-disable-next-line no-new, new-cap
+  new Sortable.default(flex2.get(), { draggable: 'span.element-added' });
   Object.keys(PromptElement).forEach((key) => {
     const el = PromptElement[key];
     const elementInput = $(`<span class="element-input" title="${el.description}" data-key="${key}"><span>${el.name}</span></span>`);
