56e57f2dea6d69ec0f6df861a7113e85f2d995fb
Scriptim <Scriptim@gmx.de>
2020-04-23 18:17:15 +0200

    Add color picker

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/* global $, Sortable, PromptElement, Prompt, EscapedPromptElement */
+/* global $, Sortable, Color, PromptElement, Prompt, EscapedPromptElement */
 
 const flex1 = $('#flex1');
 const flex2 = $('#flex2');
@@ -32,6 +32,36 @@ function addElementInputs() {
   });
 }
 
+function initColorPicker() {
+  const colorPicker = $('#color-picker');
+
+  const sectionStarts = [252, 248, 244, 240, 236, 232, 196, 160, 124, 88, 52, 16, 12, 8, 4, 0];
+  const sections = {};
+
+  sectionStarts.forEach((start) => {
+    sections[start] = $(`#color-picker-${start}`);
+  });
+
+  for (let i = 0; i < 256; i += 1) {
+    let colorName = Color[i].name;
+    if (i < 16) {
+      colorName += ` (8 bit) or ${Color[i].color16.name} (3/4 bit)`;
+    }
+    const color = $(`<span class="color-picker-color" style="background-color:${Color[i].hex}" title="${colorName}" data-color-id="${i}"></span>`);
+
+    sections[sectionStarts.find((val) => i >= val)].append(color);
+  }
+  colorPicker.click(() => false);
+  const colorPickerWrapper = $('#color-picker-wrapper');
+  colorPickerWrapper.click(() => colorPickerWrapper.hide());
+  $('.color-picker-color').click((/* event */) => {
+    // const color = Color[$(event.target).attr('data-color-id')];
+    // TODO: handle selected color
+    colorPickerWrapper.hide();
+  });
+}
+
 window.onload = () => {
   addElementInputs();
+  initColorPicker();
 };
