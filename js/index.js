899801f1b4679551131be9f6899c4f84301dbde9
Scriptim <Scriptim@gmx.de>
2020-06-05 13:48:21 +0200

    Replace quotes in descriptions with html entity

--- js/index.js
+++ js/index.js
@@ -93,7 +93,7 @@ function addElementInputs() {
 
   Object.keys(PromptElement).forEach((key) => {
     const el = PromptElement[key];
-    const elementInput = $(`<span class="element-input" title="${el.description}" data-key="${key}"><span>${el.name}</span></span>`);
+    const elementInput = $(`<span class="element-input" title="${el.description.replace(/"/g, '&quot;')}" data-key="${key}"><span>${el.name}</span></span>`);
     elementInput.click(() => {
       const removeElement = $('<!--\n  The "x" svg icon was taken from <https://github.com/refactoringui/heroicons>.\n  MIT License: <https://github.com/refactoringui/heroicons/blob/master/LICENSE>\n--><svg class="remove-element" title="Remove" fill="currentColor" viewbox="0 0 20 20"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>');
       const elementAdded = $(`<span class="element-added" title="${el.description}" data-key="${key}"><span>${el.name}</span></span>`);
