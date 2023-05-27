5faeeff5415f3b7c16fc29ec39cd71c18ee4001a
Scriptim <me@scriptim.dev>
2023-05-27 15:41:02 +0200

    Add deployment workflow for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/bash-prompt-generator/' : '/',
 });
