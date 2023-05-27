3b9fbb1229f6ceace36df854f4f9b3ea8632a635
Scriptim <me@scriptim.dev>
2023-05-27 23:03:59 +0200

    Fix public path for new domain

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,4 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/bash-prompt-generator/' : '/',
 });
